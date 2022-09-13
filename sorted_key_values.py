8482e0dc6b4177090eb5cde50fa7073df812ea85
Shega <104076420+Shega-Vala@users.noreply.github.com>
2022-09-13 10:19:05 -0400

    first folder - with the first excercise

---
+++ sorted_key_values.py
@@ -0,0 +1,9 @@
+item_dictionary = {"Apple" : 3, "Onion" : 1, "Banana" :5 }
+item_list = list(item_dictionary.items())
+
+for i in range(list):
+    take = input("Enter the item and the price Ex. Apple 2: ")
+
+
+
+ 
